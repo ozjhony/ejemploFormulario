1ee74ac79a63c8ce76972fa7aca30f149bdeb7ff
ozjhony <alejandrojpg1996@gmail.com>
2021-04-11 20:49:01 -0500

    algunas correcciones

--- validacion.js
+++ validacion.js
@@ -2,8 +2,9 @@ var Nombre=document.getElementById("Nombre");
 var Apellido=document.getElementById("Apellido");
 var Celular=document.getElementById("Celular");
 var Email=document.getElementById("Email");
-var Carrera=document.getElementById("Carrera")
+var Carrera=document.getElementById("Carrera");
 var form = document.getElementById('form');
+var fecha=document.getElementById("fecha");
 
 console.log(form);
 
@@ -19,24 +20,24 @@ form.addEventListener("submit", e=>{
         alert("ingrese un apellido valido mayor a 3 caracteres")
    } 
    else if(Email.value.length<1){
-    alert("el campo email es requerido")
+    alert("el campo email es requerido");
+   }
+   else if(isNaN(Celular.value)){
+       alert("el campo debe ser numerico");
    }
    else if(Celular.value.length<1){
-       alert("por favor ingresa tu numero celular")
+       alert("por favor ingresa tu numero celular");
    }
-   else if(Cedula.value.length<4){
-    alert("ingrese un numero de identificacion valido")
+   else if(Cedula.value.length<4 || isNaN(Cedula.value)){
+    alert("Esta cedula no es valida o el valor no es numerico");
    }
    
    else if(Carrera.value.length<1){
-    alert("El campo carrera es requerido")
+    alert("El campo carrera es requerido");
    }
-
-
-
-  else{
-     alert("formulario enviado")
-  }
+    else{
+     alert("formulario enviado");
+}
 })
 
 
